fbd0f734ba5cac72478b40e91628b68d705c3038
tw1ps <tw1ps@outlook.com>
2023-03-30 14:33:58 +0000

    fix: 使用UploadDragger时rollup无法定位es/upload-draager feat: 复制es/upload重命名

---
+++ src/views/st/dashbroad/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="grid lg:grid-cols-10 h-99% w-full sm:grid-cols-1"> </div>
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import {
+    Card,
+    Modal,
+    Statistic,
+    Carousel,
+    Descriptions,
+    DescriptionsItem,
+    Tag,
+    Tooltip,
+  } from 'ant-design-vue';
+
+  export default defineComponent({
+    name: 'Analysis',
+    components: {
+      [Card.name]: Card,
+      [Modal.name]: Modal,
+      [Statistic.name]: Statistic,
+      [Carousel.name]: Carousel,
+      [Descriptions.name]: Descriptions,
+      [DescriptionsItem.name]: DescriptionsItem,
+      [Tag.name]: Tag,
+      [Tooltip.name]: Tooltip,
+    },
+  });
+</script>
+<script lang="ts" setup>
+  import { ref, onMounted } from 'vue';
+
+  const loading = ref<boolean>(true);
+
+  setTimeout(() => {
+    loading.value = false;
+  }, 1500);
+
+  onMounted(async () => {
+    loading.value = false;
+  });
+</script>
